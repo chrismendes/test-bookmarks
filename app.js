81d1a7dbe44543eefce4ac10301deb7b18beadaa
Chris Mendes <chris@chrismendes.uk>
2020-10-17 00:55:29 +0100

    Added container/section layouts

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ import 'normalize.css';
 import './helpers/base.scss';
 
 import './layouts/Header/LayoutHeader.scss'
+import './layouts/Container/LayoutContainer.scss'
+import './layouts/Section/LayoutSection.scss'
 
 import './components/AppTitle/AppTitle.scss'
