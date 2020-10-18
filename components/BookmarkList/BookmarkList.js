9779ee99cd2e88107881f52d4cd5c6673dcb6830
Chris Mendes <chris@chrismendes.uk>
2020-10-18 16:22:14 +0100

    Added edit delete save buttons and editmode styles to bookmarks

--- components/BookmarkList/BookmarkList.js
+++ components/BookmarkList/BookmarkList.js
@@ -16,7 +16,15 @@ export default class BookmarkList {
 
     bookmarks.reverse();
     const result = bookmarks.reduce((list, bookmark) => list + `
-      <span class="bookmark"><a href="${bookmark}" target="_blank">${bookmark}</a></span>
+      <div class="bookmark">
+        <a class="bookmark_url" href="${bookmark}" target="_blank">${bookmark}</a>
+        <input class="bookmark_editurl" type="text" value="${bookmark}" />
+        <div class="bookmark_buttons">
+          <button class="_button" data-action="edit">Edit</button>
+          <button class="_button" data-action="delete">Delete</button>
+          <button class="_button" data-action="save">Save</button>
+        </div>
+      </div>
     `, '');
     this.$container.innerHTML = result;
   }
