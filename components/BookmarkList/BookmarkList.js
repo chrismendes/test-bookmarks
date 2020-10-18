90318c3a7b365d9662fad7e05bd0b9486c7ab033
Chris Mendes <chris@chrismendes.uk>
2020-10-18 16:32:39 +0100

    Added button styles and added to bookmark edit buttons

--- components/BookmarkList/BookmarkList.js
+++ components/BookmarkList/BookmarkList.js
@@ -20,9 +20,9 @@ export default class BookmarkList {
         <a class="bookmark_url" href="${bookmark}" target="_blank">${bookmark}</a>
         <input class="bookmark_editurl" type="text" value="${bookmark}" />
         <div class="bookmark_buttons">
-          <button class="_button" data-action="edit">Edit</button>
-          <button class="_button" data-action="delete">Delete</button>
-          <button class="_button" data-action="save">Save</button>
+          <button class="button button-secondary" data-action="edit">Edit</button>
+          <button class="button button-last" data-action="delete">Delete</button>
+          <button class="button" data-action="save">Save</button>
         </div>
       </div>
     `, '');
