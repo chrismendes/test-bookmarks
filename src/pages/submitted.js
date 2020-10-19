f429d91e90076f347ab0e9afdcaf3a2703de8924
Chris Mendes <chris@chrismendes.uk>
2020-10-19 13:13:31 +0100

    Set back to index button to bookmark page 1

--- src/pages/submitted.js
+++ src/pages/submitted.js
@@ -22,7 +22,7 @@ export default class SubmittedPage {
             <h2 class="memo_title">Bookmark Submitted</h2>
             <span class="memo_msg">Your bookmark <a href="${lastBookmark}" target="_blank">${lastBookmark}</a> has been added.</span>
             <span class="memo_button">
-              <a href="/#/" class="button">Back to bookmark list</a>
+              <a href="/#/1" class="button">Back to bookmark list</a>
             </span>
           </div>
         </div>
